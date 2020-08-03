7ecb32c6b6368dca8edf9b4d846ad421a846a325
Miguel Patrocinio <jose.m.patrocinio@hotmail.com>
2020-08-03 22:26:00 +0200

    building the Portfolio component / connecting the Portfolio with vuex

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -1,3 +1,51 @@
 <template>
-    <h1>portfolio/stock component</h1>
+    <div class="col-sm-6 col-md-4">
+        <div class="panel panel-success">
+            <div class="panel-heading">
+                <h3 clas="panel-title">
+                    {{stock.name}}
+                    <small>(Price: {{stock.price}} | Quantity: {{stock.quantity}})</small>
+                </h3>
+            </div>
+            <div class="panel-body">
+                <div class="pull-left">
+                    <input 
+                        type="number"
+                        class="form-control"
+                        placeholder="quantity"
+                        v-model="quantity">
+                </div>
+                <div class="pull-right">
+                    <button 
+                    class="btn btn-success"
+                    @click="sellStock"
+                    :disabled="quantity <= 0 || Number.isInteger(quantity)">SELL</button>
+                </div>
+            </div>
+
+        </div>
+    </div>
 </template>
+
+
+
+<script>
+export default {
+    props: ['stock'],
+    data() {
+        return {
+            quantity: 0
+        }
+    },
+    methods: {
+     sellStock() {
+         const order = {
+             stockId: this.stock.id,
+             stockprice: this.stock.price,
+             quantity: this.quantity
+         }
+        this.$store.dispatch('sellStock', order)
+     }
+    }
+}
+</script>
--- src/store/modules/portfolio.js
+++ src/store/modules/portfolio.js
@@ -6,10 +6,8 @@ const state = {
 
 const mutations = {
     "BUY_STOCK" (state, payload) {
-        console.log("payload(ORDER) MOSULES/PORTFOLIO", payload);
-        console.log(payload.stockId);
-        
-        
+        // console.log("payload(ORDER) MOSULES/PORTFOLIO", payload);
+  
         const record = state.stocks.find(el => el.id === payload.stockId)
         // console.log("record", record);
         
@@ -24,49 +22,57 @@ const mutations = {
         }
         state.funds -= payload.stockPrice * payload.quantity
 
-        console.log("state in portfolio", state);
+        // console.log("state in portfolio", state);
         
     },
-    // 'SELL_STOCK' (state, payload) {
-    //     const record = state.stocks.find(el => el.id === payload.stockId)
+    'SELL_STOCK' (state, payload) {
 
-    //     if (record.quantity > quantity) {
-    //         record.quantity -= quantity
-    //     } else {
-    //         state.stocks.splice(state.stocks.indexOf(record), 1)
-    //     }
-    //     state.funds += stockPrice * quantity
-    // }
+        // console.log("payload in sell_stock", payload);
+        
+        const record = state.stocks.find(el => el.id === payload.stockId)
+
+        if (record.quantity > quantity) {
+            record.quantity -= quantity
+        } else {
+            state.stocks.splice(state.stocks.indexOf(record), 1)
+        }
+        state.funds += stockPrice * quantity
+    }
 }
 
 
-// const actions = {
-//     sellStock({commit}, payload) {
-//         commit('SELL_STOCK', payload)
-//     }
-// }
+const actions = {
+    sellStock({commit}, payload) {
+        commit('SELL_STOCK', payload)
+    }
+}
 
-// const getters = {
-//     stockPortfolio (state, payload) {
-//         return state.stocks.map(stock => {
-//             const record = payload.stocks.find(el => el.id == stock.id)
-//             return {
-//                 id: stock.id,
-//                 quantity: stock.quantity,
-//                 name: record.name,
-//                 price: record.price
-//             }
-//         })
-//     },
-//     funds (state) {
-//         return state.funds
-//     }
-// }
+const getters = {
+    stockPortfolio (state, payload) {
+
+        console.log("payload in stockportfolio", payload);
+        
+        return state.stocks.map(stock => {
+            const record = payload.stocks.find(el => el.id == stock.id)
+            console.log(record);
+            
+            return {
+                id: stock.id,
+                quantity: stock.quantity,
+                name: record.name,
+                price: record.price
+            }
+        })
+    },
+    funds (state) {
+        return state.funds
+    }
+}
 
 
 export default {
     state,
     mutations,
-    // actions,
-    // getters
+    actions,
+    getters
 }
