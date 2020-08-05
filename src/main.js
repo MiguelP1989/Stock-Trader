d2b7cdc90edbba976bd7a44cf6067ffbb7d7b9cd
Miguel Patrocinio <jose.m.patrocinio@hotmail.com>
2020-08-05 15:08:06 +0200

    making dropdown open an close / setting up vue-resource

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from "vue-router"
+import VueResource from 'vue-resource'
 
 import {store} from "./store/store" 
 import { routes } from "./routes"
 
 
 Vue.use(VueRouter);
+Vue.use(VueResource)
+
+Vue.http.options.root = 'https://stock-trader-ef3c9.firebaseio.com/'
 
 Vue.filter('currency', (value) => {
   return '£' + value.toLocaleString()
